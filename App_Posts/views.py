c240822537b5829bccae68131802dd4adc19ca5a
Abir <mdtanimurabir0@gmail.com>
2024-09-26 12:01:50 +0600

    display posted images

--- App_Posts/views.py
+++ App_Posts/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from App_Login.models import UserProfile
+from App_Login.models import UserProfile, Follow
 from django.contrib.auth.models import User
+
+from App_Posts.forms import Posts
+
 # Create your views here.
 
 @login_required
 def home(request):
+    following_list =Follow.objects.filter(follower=request.user)
+    postss = Posts.objects.filter(author__in=following_list.values_list('following'))
     if request.method == 'GET':
         search = request.GET.get('search', '')
         result = User.objects.filter(username__icontains=search)
-    return render(request, 'App_Posts/home.html', context={'title':'Home', 'search':search, 'result':result})
+    return render(request, 'App_Posts/home.html', context={'title':'Home', 'search':search, 'result':result, 'following_list':following_list, 'postss':postss})
 
