794eed3c20bccd1b86ac4cf197b99a1a0892ddb4
Abir <mdtanimurabir0@gmail.com>
2024-10-01 12:15:31 +0600

    changes

--- Social_Project/urls.py
+++ Social_Project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns #static, staticfiles_urlpattern
+from django.contrib.staticfiles.urls import  staticfiles_urlpatterns #static, staticfiles_urlpattern
 from App_Posts import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
